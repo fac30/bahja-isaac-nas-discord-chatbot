4bf1fe23de688d2575f7010c53f36009832151e6
“bajii2020” <bajjahmed22@gmail.com>
2024-02-13 22:38:55 +0000

    updated files

--- index.js
+++ index.js
@@ -1,3 +1,111 @@
-require('dotenv/config');
-const { Client } = require('discord.js');
+//importing discord.js openAI and dotenv
+require('dotenv/config')
+const { Client, GuildMember } = require('discord.js');
+const { OpenAI} = require('openai'); //import client class
+
+const client = new Client({
+    intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent']
+})
+
+client.on('ready', () => {
+    console.log('The bot is online!')
+}) 
+
+const IGNORE_PREFIX = "!";
+
+//The channels we want the bot to respond in
+const CHANNELS = ['1205838668598870037','1205838668598870040']
+
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY,
+})
+
+//listening for messages from discord
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(IGNORE_PREFIX)) return;
+    //if message doesn't include channel ids and message doesnt inclue ping to bot then we will ignore the message.
+    if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
+
+
+    //bot is typing 
+    await message.channel.sendTyping(); 
+    //set interval
+    const sendTypingInterval = setInterval(() => {
+        message.channel.sendTyping();
+    }, 5000);
+
+
+
+    //fetch all past messages 
+
+    let conversation = [];
+    conversation.push({
+        role: 'system',
+        content: 'Chat GPT is your bot friend.'
+    });
+
+    let prevMessages = await message.channel.messages.fetch({ limit: 10 });//will fetch last 10 messages from the channel
+    prevMessages.reverse();// correct order
+
+    //loop through each prev messages to push to the convo array
+    prevMessages.forEach((msg) => {
+        //if the msg auth was a bot and the id does not match our bot id then we'll ignore the msg.
+        if(msg.author.bot && msg.author.id !== client.user.id) return;
+        if(msg.content.startsWith(IGNORE_PREFIX)) return;
+
+        //evaluate user name of person, remove spaces etc
+        const username = msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
+        
+        //check msg auther id
+        if (msg.author.id === client.user.id) {
+            conversation.push({
+                role: 'assistant',
+                name: username,
+                content: msg.content,
+            });
+            return;
+        }
+
+        //this will run if the convo belongs to a regular user
+        conversation.push({
+            role: 'user',
+            name: 'username',
+            content: msg.content,
+        })
+
+    });
+
+
+
+//responde to messge by sending a request to openAI
+const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: conversation,
+
+}).catch((erros) => console.error('OpenAI Error:\n', error));
+
+
+clearInterval(sendTypingInterval);
+
+if(!response) {
+    message.reply('Open AI not responding. Try again soon.');
+    return;
+}
+
+//breakdown message chara because of discord limit
+
+const respondeMessage = response.choices[0].message.content;
+const chunkSizeLimit = 2000;
+
+for (let i =0; i < respondeMessage.length; i+= chuckSizeLimit) {
+    const chunk = respondeMessage.substring(i, i + chunkSizeLimit);
+}
+
+  await message.reply(chunk);
+
+});
+
+client.login(process.env.TOKEN);
 
