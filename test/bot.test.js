4d3ea6c03d32b47bbfde50bdf34cff0fb2c70404
Nas <n.choudhury1@live.co.uk>
2024-02-22 20:42:39 +0000

    updated requirements and cleaned up file

--- test/bot.test.js
+++ test/bot.test.js
@@ -2,22 +2,4 @@ const { describe, it } = require('node:test');
 const assert = require('assert');
 const { client } = require('../src/bot');  
 
-describe('Discord Bot Tests', () => {
-  it('should have a valid Discord.js client instance', () => {
-    assert.ok(client instanceof require('discord.js').Client);
-  });
 
-  it('should emit the "ready" event', (done) => {
-    // Assuming your bot emits the "ready" event when it's ready
-    client.once('ready', () => {
-      assert.ok(true);  
-      done();
-    });
-  });
-
-  it('should have the correct intents enabled', () => {
-    const actual = client;
-    const expected = ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'];
-    assert.ok(actual, expected);
-  });
-});
