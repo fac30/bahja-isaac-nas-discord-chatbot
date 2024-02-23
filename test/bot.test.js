c0c902b22687a356eb972d735c9897dd3a8ffb2e
Nas <n.choudhury1@live.co.uk>
2024-02-23 19:38:07 +0000

    refactored code, still working correctly

--- test/bot.test.js
+++ test/bot.test.js
@@ -3,13 +3,13 @@ const { client } = require('../src/bot');
 /*****  
 mock data 
 *****/ 
-const passClientKeyObject =  [ "_events", "_eventsCount", "_maxListeners", "options", "rest", "ws", "actions", "voice", "shard", "users", "guilds", "channels", "sweepers", "presence", "user", "application", "readyTimestamp"];
+const pass_client_key_object =  [ "_events", "_eventsCount", "_maxListeners", "options", "rest", "ws", "actions", "voice", "shard", "users", "guilds", "channels", "sweepers", "presence", "user", "application", "readyTimestamp"];
 
-const failClientKeyObject =  [ "_events", "_eventsCount", "_maxListeners", "options", "rest", "ws", "actions", "voice", "shard", "users", "sweepers", "presence", "user", "application", "readyTimestamp"];
+const fail_client_key_object =  [ "_events", "_eventsCount", "_maxListeners", "options", "rest", "ws", "actions", "voice", "shard", "users", "sweepers", "presence", "user", "application", "readyTimestamp"];
 
 describe('Discord Bot Tests - happy path', () => {
   it('should have a valid Discord.js client instance', () => {
-    expect(client instanceof require('discord.js').Client).ok;
+    expect(client instanceof require('discord.js').Client).toBeTruthy();
   });
 
   it('should emit the "ready" event', (done) => {
@@ -22,7 +22,7 @@ describe('Discord Bot Tests - happy path', () => {
 
   it('should return the correct client keys', () => {
     const actual = Object.keys(client);
-    expect(actual).toMatchObject(passClientKeyObject);
+    expect(actual).toMatchObject(pass_client_key_object);
   });
 
   it('checks that the client key in not undefined', () => {
@@ -43,6 +43,6 @@ describe('Discord Bot clientKeyObject - unhappy path', () => {
 
   it('should fail to match and return an error', () => {
     const actual = Object.keys(client);
-    expect(actual).not.toBe(failClientKeyObject);
+    expect(actual).not.toBe(fail_client_key_object);
   });
 })
