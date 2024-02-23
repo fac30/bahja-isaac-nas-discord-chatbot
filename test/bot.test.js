af670d42d54caddff0a5adab2187076d7d56e09c
Nas <n.choudhury1@live.co.uk>
2024-02-23 00:14:31 +0000

    added a failing test for the object

--- test/bot.test.js
+++ test/bot.test.js
@@ -1,11 +1,16 @@
 const { client } = require('../src/bot');  
 
+// mock data
+const clientKeyObject =  [ "_events", "_eventsCount", "_maxListeners", "options", "rest", "ws", "actions", "voice", "shard", "users", "guilds", "channels", "sweepers", "presence", "user",   "application", "readyTimestamp"];
+
+const failClientKeyObject =  [ "_events", "_eventsCount", "_maxListeners", "options", "rest", "ws", "actions", "voice", "shard", "users", "sweepers", "presence", "user",   "application", "readyTimestamp"];
+
 describe('Discord Bot Tests', () => {
-  test('should have a valid Discord.js client instance', () => {
+  it('should have a valid Discord.js client instance', () => {
     expect(client instanceof require('discord.js').Client).ok;
   });
 
-  test('should emit the "ready" event', (done) => {
+  it('should emit the "ready" event', (done) => {
     // Assuming your bot emits the "ready" event when it's ready
     client.once('ready', () => {
       expect(true).toBeTruthy(); 
@@ -13,9 +18,15 @@ describe('Discord Bot Tests', () => {
     });
   });
 
-  test('should return teh correct client keys', () => {
+  it('should return the correct client keys', () => {
     const actual = Object.keys(client);
-    const expected =  [ "_events", "_eventsCount", "_maxListeners", "options", "rest", "ws", "actions", "voice", "shard", "users", "guilds", "channels", "sweepers", "presence", "user",   "application", "readyTimestamp"];
-    expect(actual).toMatchObject(expected);
+    expect(actual).toMatchObject(clientKeyObject);
   });
 });
+
+describe('Discord Bot clientKeyObject', () => {
+  it('should fail to match and return an error', () => {
+    const actual = Object.keys(client);
+    expect(actual).not.toBe(failClientKeyObject);
+  })
+})
